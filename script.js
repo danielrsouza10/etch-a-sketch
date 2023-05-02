d81b696f1efeae891c1187ec62605d1baad542e0
Daniel Souza <danielrsouza10@gmail.com>
2023-05-02 10:09:28 -0300

    Added the buttons to select the grid and create a function to size the grid after the user choice a button

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
 //grid
 const container = document.querySelector('#container');
 
-//creating the grid
+
+//default grid
 for (i = 0; i < 400; i++) {
     const grid = document.createElement('div');
     grid.className = 'box';
-    grid.style['min-width'] = '25px';
+    grid.style.width = '25px';
     grid.style.height = '25px';
     grid.style.border = '.1px solid lightgray';
     grid.style.backgroundColor = 'white';
     document.querySelector('#container').appendChild(grid);
-
 }
 
 //coloring
-
-const addColor = document.querySelectorAll('div.box');
+const addColor = document.querySelectorAll('.box');
 
 addColor.forEach((box) => {
-    addEventListener('mousemove', function (e){
-        console.log(e.target);
+    container.addEventListener('mouseover', function (e) {
+
         e.target.style.backgroundColor = 'pink';
     });
 });
 
+// size gride buttons
+const buttons = document.querySelector('#buttons');
+let choice = 0;
+
+buttons.addEventListener('click', function (e) {
+    choice = e.target.innerText;
+    createGrid(choice);
+    console.log(choice);
+})
+
+// creating other grids
+function createGrid(size) {
+    let result = 0;
+    //verifing the return of the button that was pressed
+    if (choice === '20x20') {
+        size = 20 * 20;
+        result = '25px';
+    } else if (choice === '50x50') {
+        size = 50 * 50;
+        result = '10px'
+    } else {
+        size = 100 * 100;
+        result = '5px';
+    }
+
+    for (i = 0; i < size; i++) {
+        const grid = document.createElement('div');
+        grid.className = 'box';
+        grid.style.width = result;
+        grid.style.height = result;
+        grid.style.border = '.1px solid lightgray';
+        grid.style.backgroundColor = 'white';
+        document.querySelector('#container').appendChild(grid);
+
+    }
+}
 
 
 
