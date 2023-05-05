89b2a653ca368d5d746d1a4b052467f71cc8efb1
Daniel Souza <danielrsouza10@gmail.com>
2023-05-05 08:53:20 -0300

    Just fixed the function to create a new grid and moving foward with the rainbow button

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-//grid
-const container = document.querySelector('#container');
-
-
 //default grid
+const container = document.querySelector('#container');
+const grid = document.createElement('div');
+grid.className = 'grid';
+container.appendChild(grid);
+//loop to create the default grid
 for (i = 0; i < 400; i++) {
-    const grid = document.createElement('div');
-    grid.className = 'box';
-    grid.style.width = '25px';
-    grid.style.height = '25px';
-    grid.style.border = '.1px solid lightgray';
-    grid.style.backgroundColor = 'white';
-    document.querySelector('#container').appendChild(grid);
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.style.width = '25px';
+    box.style.height = '25px';
+    box.style.border = '.1px solid lightgray';
+    box.style.backgroundColor = 'white';
+    document.querySelector('.grid').appendChild(box);
 }
 
 //colors-table
@@ -29,21 +30,18 @@ pickColor.addEventListener('click', function (e) {
     console.log(e.target.value);
 })
 
-
-
+//function to select a color
 function colorSelect(color) {
     //if rainbow button was pressed
     if (color === 'rainbow') {
         colorChoice = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    //if gradient color was pressed
+        //if a color was select in the gradient
     } else {
         colorChoice = color;
     }
     return colorChoice;
- 
+
 }
-
-
 
 //coloring
 const addColor = document.querySelectorAll('.box');
@@ -51,7 +49,7 @@ const addColor = document.querySelectorAll('.box');
 addColor.forEach((box) => {
     container.addEventListener('mouseover', function (e) {
 
-        e.target.style.backgroundColor = colorChoice;
+        e.target.style.backgroundColor = colorSelect('rainbow');
     });
 });
 
@@ -60,13 +58,21 @@ const buttons = document.querySelector('#buttons');
 let choice = 0;
 
 buttons.addEventListener('click', function (e) {
+    const grid = document.querySelector('.grid');
+    container.removeChild(grid);
     choice = e.target.innerText;
-    createGrid(choice);
+    const newGrid = document.querySelector('.box');
+    createNewGrid(choice);
     console.log(choice);
 })
 
 // creating other grids
-function createGrid(size) {
+function createNewGrid(size) {
+    const container = document.querySelector('#container');
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+    container.appendChild(grid);
+
     let result = 0;
     //verifing the return of the button that was pressed
     if (choice === '20x20') {
@@ -74,30 +80,34 @@ function createGrid(size) {
         result = '25px';
     } else if (choice === '50x50') {
         size = 50 * 50;
-        result = '10px'
+        result = '10px';
     } else {
         size = 100 * 100;
         result = '5px';
     }
 
+    
+
     for (i = 0; i < size; i++) {
-        const grid = document.createElement('div');
-        grid.className = 'box';
-        grid.style.width = result;
-        grid.style.height = result;
-        grid.style.border = '.1px solid lightgray';
-        grid.style.backgroundColor = 'white';
-        document.querySelector('#container').appendChild(grid);
+
+
+        //create new div
+        const newGrid = document.createElement('div');
+        newGrid.className = 'newBox';
+        newGrid.style.width = result;
+        newGrid.style.height = result;
+        newGrid.style.border = '.1px solid lightgray';
+        newGrid.style.backgroundColor = 'white';
+        document.querySelector('.grid').appendChild(newGrid);
 
     }
+    //remove previous grid
+    // const grid = document.querySelector('.box');
+    // grid.remove('.box');
+    // console.log(size);
 }
 
-// function selectColor(){
-//     const colorSelect = document.querySelectorAll('.color');
-//     let colorChoice;
-//     colorChoice = colorSelect.innerText;
-//     console.log(colorChoice);
-// }
+
 
 
 
